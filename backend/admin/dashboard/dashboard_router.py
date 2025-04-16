8913595c603110b998e82c65a3c56ac4ba455001
j1nwan <weamicron@gmail.com>
2025-04-16 21:44:03 +0900

    feat: 관리자 공통 레이아웃 분리

--- backend/admin/dashboard/dashboard_router.py
+++ backend/admin/dashboard/dashboard_router.py
@@ -1,33 +1,16 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Request, status
 from fastapi.responses import JSONResponse
-from fastapi.templating import Jinja2Templates
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 import logging
+from config.templates import templates
 
 # 로거 설정
 logger = logging.getLogger(__name__)
 
 # 라우터 정의
-router = APIRouter()
+router = APIRouter(tags=["대시보드"])
 
-templates = Jinja2Templates(directory="frontend/view/admin")
-
-# API 전용 : JSON 반환
-@router.get("/api/dashboard", status_code=status.HTTP_201_CREATED)
-async def dashboard_json_data():
-    try:
-        return ''
-    except HTTPException as e:
-        # HTTPException은 그대로 전파
-        raise e
-    except Exception as e:
-        logger.exception(f"회원가입 처리 중 예상치 못한 오류: {str(e)}")
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="회원가입 처리 중 오류가 발생했습니다."
-        )
-
-# API 전용 : JSON 반환
-@router.get("/admin/dashboard", status_code=status.HTTP_201_CREATED)
-async def dashboard_view():
-    return templates.TemplateResponse("dashboard.html")
+# 관리자 페이지
+@router.get("/admin/dashboard")
+async def admin_dashboard(request: Request):
+    return templates.TemplateResponse("admin/dashboard.html", {"request": request})
